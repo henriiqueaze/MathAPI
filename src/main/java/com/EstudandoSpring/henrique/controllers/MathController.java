829914246c8d498c9c923b48d1f42893b95c59dc
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2024-10-29 15:42:45 -0300

    Adicionando operações na calculadora

--- src/main/java/com/EstudandoSpring/henrique/controllers/MathController.java
+++ src/main/java/com/EstudandoSpring/henrique/controllers/MathController.java
@@ -1,6 +1,7 @@
 package com.EstudandoSpring.henrique.controllers;
 
 import com.EstudandoSpring.henrique.Exceptions.UnsuportedMathOperationException;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +15,47 @@ public class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsuportedMathOperationException("Please set a numeric value!");
         }
-
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("sub/{numberOne}/{numberTwo}")
+    public Double sub(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("mul/{numberOne}/{numberTwo}")
+    public Double mul(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("div/{numberOne}/{numberTwo}")
+    public Double div(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+       return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("media/{numberOne}/{numberTwo}")
+    public Double media(@PathParam(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping("sqrt/{numberOne}/{numberTwo}")
+    public String sqrt(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+        return "Number One:" + Math.sqrt(convertToDouble(numberOne)) + "\nNumber Two: " + Math.sqrt(convertToDouble(numberTwo));
     }
 
     private Double convertToDouble(String strNumber) throws Exception {
