6c4701081bd3f6d56f0dee5d40640c8e1ef4c470
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2024-10-29 13:48:25 -0300

    Adicionando arquivos

---
+++ src/test/java/com/EstudandoSpring/henrique/HenriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EstudandoSpring.henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
